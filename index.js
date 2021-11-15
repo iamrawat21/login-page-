084ff4fc02cb37757bd8fa00fce195adcfbc272c
Devansh <90498266+iamrawat21@users.noreply.github.com>
2021-11-15 12:47:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/" , function(req , res){
+  res.sendFile(__dirname + "/signup.html");
+});
+app.post("/" , function(req , res){
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "Subscribed",
+        merge_field:{
+          FNAME: firstName,
+          LNAME: lastName,
+
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+
+  const url= "https://us20.api.mailchimp.com/3.0/lists/c406b17837";
+
+  const options = {
+    method: "POST",
+    auth : "iamdrawat21  264dea01c8f1a1751ca597f4e72a7b57-us20"
+  };
+const request = https.request(url, options, function(response){
+  if(response.statusCode===200){
+    res.sendFile(__dirname , "/success.html");
+  }  else{
+    res.sendFile(__dirname + "/failure.html"); 
+  }
+  response.on("data" , function(data){
+      console.log(JSON.parse(data));
+    });
+});
+  request.write(jsonData);
+  request.end();
+});
+
+app.listen(3000 , function(){
+  console.log("The server is running in 3000");
+});
